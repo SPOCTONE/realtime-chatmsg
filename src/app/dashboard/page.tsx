d1d5dd629ddc0d1d467016814b02ca7e6e962080
Subham Patel <47655017+SPOCTONE@users.noreply.github.com>
2023-07-13 17:45:36 +0530

    Initial commit
    Complete project till authentication.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <button>Hello</button>
+}
+
+export default page
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Button from "@/components/ui/Button"
+import { db } from "../lib/db"
+
+export default async function Home() {
+
+return <Button>Hello</Button>
+  
+  return <div className='text-red-500'>Hello World!</div>
+}
